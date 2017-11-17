0622546fa7c544bdc72b58f8b91823175aad305d
Tim DiSabatino <tim@pathwaysdigital.com>
2017-11-17 13:54:31 -0800

    Add sign in link & session routes, controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
+  root "movies#index"
   resources :users
   get "signup" => "users#new"
-  root "movies#index"
+
   resources :movies do
     resources :reviews
   end
+
+  resource :session
 end
