ba22ca608ec6424b6801914eee48f52864792c13
Tim DiSabatino <tim@pathwaysdigital.com>
2017-11-22 16:18:13 -0800

    Restrict user access authorization

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  private
+
+  def require_signin
+    unless current_user
+      session[:intended_url] = request.url
+      redirect_to new_session_url, alert: "Please sign in first!"
+    end
+  end
+
+  def current_user
+    User.find(session[:user_id]) if session[:user_id]
+  end
+
+  helper_method :current_user
+
+  def current_user?(user)
+    current_user == user
+  end
+
+  helper_method :current_user?
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  before_action :require_signin, except: [:new, :create]
+  before_action :require_correct_user, only: [:edit, :update, :destroy]
 
   def index
     @users = User.all
@@ -23,11 +25,9 @@ class UsersController < ApplicationController
 end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-  @user = User.find(params[:id])
   if @user.update(user_params)
     redirect_to @user, notice: "Account successfully updated!"
   else
@@ -36,7 +36,6 @@ end
 end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
     session[:user_id] = nil
     redirect_to root_url, alert: "Account successfully deleted!"
@@ -48,4 +47,10 @@ end
     params.require(:user).
     permit(:name, :email, :password, :password_confirmation)
   end
+
+  def require_correct_user
+   @user = User.find(params[:id])
+   redirect_to root_url unless current_user?(@user)
+ end
+
 end
