abfb0c74928ac154c42ea5ebddc6ce4fe9fd9cfd
Tim DiSabatino <tim@pathwaysdigital.com>
2017-12-01 10:02:55 -0800

    Add custom routes for filters

--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,10 +1,6 @@
 module MoviesHelper
   def format_total_gross(movie)
-    if movie.flop?
-      content_tag(:strong, 'Flop!')
-    else
       number_to_currency(movie.total_gross)
-    end
   end
 
   def image_for(movie)
@@ -19,7 +15,7 @@ module MoviesHelper
     image_tag('trash.png', style: 'border: 0', title: 'Delete Review')
   end
 
-  def format_average_stars(movie)   
+  def format_average_stars(movie)
     if movie.average_stars.nil?
       content_tag(:strong, 'No reviews')
     else
--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,9 @@ Rails.application.routes.draw do
   resources :users
   get "signup" => "users#new"
 
+  #get "movies/filter/hits" => "movies#index", scope: "hits"
+  #get "movies/filter/flops" => "movies#index", scope: "flops"
+  get 'movies/filter/:scope' => "movies#index", as: :filtered_movies
   resources :movies do
     resources :reviews
     resources :favorites
